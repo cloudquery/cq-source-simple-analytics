7892b4d04bca983bc592f2d7d3f9ec030e68ceb6
Herman Schaaf <hermanschaaf@gmail.com>
2023-01-26 15:19:03 +0000

    Use faker for resource tests (#5)

--- resources/page_views_test.go
+++ resources/page_views_test.go
@@ -1,13 +1,32 @@
 package resources
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/cloudquery/cq-source-simple-analytics/client"
+	"github.com/cloudquery/cq-source-simple-analytics/internal/simpleanalytics"
+	"github.com/cloudquery/plugin-sdk/faker"
 )
 
 func TestPageViews(t *testing.T) {
-	ts := testServer(t, "page_views.ndjson")
+	var pv simpleanalytics.PageView
+	if err := faker.FakeObject(&pv); err != nil {
+		t.Fatal(err)
+	}
+	pv.Metadata = map[string]any{
+		"metadata.foo_text": "bar",
+		"metadata.bar_int":  123,
+		"metadata.baz_date": "2021-01-01T00:00:00Z",
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/x-ndjson")
+		d, _ := json.Marshal(pv)
+		_, _ = w.Write(d)
+	}))
 	defer ts.Close()
+
 	client.TestHelper(t, PageViews(), ts)
 }
